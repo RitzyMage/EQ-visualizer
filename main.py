4dd078a935fb0b7a7188bbf868bba3ea0989cd0a
Avery Green <averyg@utahcounty.gov>
2022-12-30 13:48:50 -0700

    kinda janky gaussian

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from scipy.io import wavfile
 from scipy.fftpack import fft
+from scipy import ndimage
 from numpy import average, absolute
 import numpy as np
 import math
@@ -29,15 +30,19 @@ FRAME_COUNT = int(FPS*SECONDS)
 fourcc = VideoWriter_fourcc(*'MP42')
 video = VideoWriter('./noise.avi', fourcc, float(FPS), (WIDTH, HEIGHT))
 
+channels = []
+
+## initialize channels
+
 for frameIndex in range(FRAME_COUNT):
     time = frameIndex / FPS
-    print('on frame', frameIndex, 'time:\t', int(time // 60), '\t:\t', int(time % 60))
+    print('creating channel on frame', frameIndex, 'time:\t', int(time // 60), '\t:\t', int(time % 60))
     sampleStartTime = time - (SAMPLE_LENGTH / 2)
     sampleEndTime = time + (SAMPLE_LENGTH / 2)
     sampleStartIndex = max(0, int(sampleStartTime * rate))
     sampleEndIndex = min(int(sampleEndTime * rate), len(data))
 
-    channels = np.full(NUM_CHANNELS, 0, dtype=float)
+    channelsForFrame = np.full(NUM_CHANNELS, 0, dtype=float)
     frequencies = absolute(fft(data[sampleStartIndex:sampleEndIndex,LEFT]))
     frequencies = frequencies[:len(frequencies) // 2] # ignore negative frequencies
 
@@ -60,10 +65,26 @@ for frameIndex in range(FRAME_COUNT):
     for i, (start, end) in enumerate(sliceRanges):
         sliced = audibleFrequencies[start:end]
         channel = min(1, (float(average(sliced)) / MAX_SAMPLE_VALUE))
-        channels[i] = channel
+        channelsForFrame[i] = channel
+    
+    channels.append(channelsForFrame)
 
+## process channels
+
+channels = ndimage.gaussian_filter(channels, sigma= 4)
+
+# for i, channelsForFrame in enumerate(channels):
+#     time = i / FPS
+#     print('processing channel on frame', i, 'time:\t', int(time // 60), '\t:\t', int(time % 60))
+#     channels[i] = scipy.signal.convolve(channelsForFrame, [0.1, 0.2, 0.2, 0.4, 0.2, 0.2, 0.1])
+
+## output video
+
+for i, channelsForFrame in enumerate(channels):
+    time = i / FPS
+    print('creating video on frame', i, 'time:\t', int(time // 60), '\t:\t', int(time % 60))
     frame = np.full((HEIGHT, WIDTH, 3), BACKGROUND_COLOR, dtype=np.uint8)
-    for i, channel in enumerate(channels):
+    for i, channel in enumerate(channelsForFrame):
         cv2.rectangle(frame, (int(i * CHANNEL_WIDTH), HEIGHT), (int((i+1) * CHANNEL_WIDTH), HEIGHT - int(HEIGHT * channel)), BAR_COLOR, int(-1))
     video.write(frame)
 video.release()
